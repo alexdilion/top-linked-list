bdd3fa41e54e5f36783deb66ca0fb81a4178747a
alexdilion <alexdilion@protonmail.com>
2023-12-26 22:18:58 +0000

    Add get/set methods for nodes

--- node.js
+++ node.js
@@ -1,7 +1,26 @@
 #!/usr/bin/env node
 
 const Node = () => {
-    return {};
+    let value = null;
+    let pointer = null;
+
+    const getValue = () => value;
+    const getPointer = () => pointer;
+
+    const setValue = (newValue) => {
+        value = newValue;
+    };
+
+    const setPointer = (newPointer) => {
+        pointer = newPointer;
+    };
+
+    return {
+        getValue,
+        getPointer,
+        setValue,
+        setPointer,
+    };
 };
 
 export default Node;
