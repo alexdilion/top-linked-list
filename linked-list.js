cff5e7c4893a928fbd107126133675270896ba12
alexdilion <alexdilion@protonmail.com>
2023-12-26 22:38:39 +0000

    Add linked-list append method

--- linked-list.js
+++ linked-list.js
@@ -3,7 +3,22 @@
 import Node from "./node.js";
 
 const LinkedList = () => {
-    return {};
+    let head = null;
+    let tail = null;
+
+    const append = (value) => {
+        let node = Node(value);
+
+        if (!head) {
+            head = node;
+        } else {
+            tail.setPointer(node);
+        }
+
+        tail = node;
+    };
+
+    return { append };
 };
 
 export default LinkedList;
